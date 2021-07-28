2f075f1492a5d8d7533fec0fe8a87d21c0ef7b5d
coder-sky <87357461+coder-sky@users.noreply.github.com>
2021-07-28 16:51:38 +0530

    Day 1 Assignment Submited

---
+++ Assigment Day1.py
@@ -0,0 +1,7 @@
+x =[int(i) for i in input('Enter Numbers: ').split()]
+#x = [1,222,21,45,34]
+print('Users_Input: ',x)
+x.sort()
+print('List in Ascending Order: ' ,x)
+x.reverse()
+print('List in Desending Order: ',x)
