8467e6661e98174a79d7545ce4f94e0866993c00
coder-sky <87357461+coder-sky@users.noreply.github.com>
2021-07-31 22:19:58 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,6 +1,6 @@
-cel = float(input("Enter the tempreture in Degree Celsious: "))
+#Please develop a python based degree converter which takes °C value and give you output of °F
 
-#C × 9/5) + 32 = ___°F
+cel = float(input("Enter the tempreture in Degree Celsious: "))
 
 Fer = cel * (9/5) + 32
 
