07950ab7ae74207914931cca291eff8ba68ccb9f
coder-sky <87357461+coder-sky@users.noreply.github.com>
2021-07-31 22:21:11 +0530

    Project Submitted

---
+++ Project.py
@@ -0,0 +1,7 @@
+#Please develop a python based degree converter which takes °C value and give you output of °F
+
+cel = float(input("Enter the tempreture in Degree Celsious: "))
+
+Fer = cel * (9/5) + 32
+
+print("Degree Frainite",Fer)
